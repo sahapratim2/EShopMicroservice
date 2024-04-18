2b7e1f7565a5b256aabf6c14f2d53f5775ee4bbb
Partha Pratim Saha <sahapratim2@gmail.com>
2024-04-18 04:08:37 -0500

    Section 7 Cross Cutting Concern

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -4,10 +4,38 @@ namespace Catalog.API.Products.CreateProduct
     public record CreateProductCommand(string Name, List<String> Category, string Description, string ImageFile, decimal Price )
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(command => command.Name).NotEmpty().WithMessage("Name is Reqired")
+                                            .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+            RuleFor(command => command.Category).NotEmpty().WithMessage("Category is Reqired");
+            RuleFor(command => command.ImageFile).NotEmpty().WithMessage("ImageFile is Reqired");
+            RuleFor(command => command.Price).GreaterThan(0).WithMessage("Price must be  Greater Than 0");
+        }
+    }
+
+    internal class CreateProductCommandHandler(IDocumentSession session
+                                       // ,IValidator<CreateProductCommand> validator,
+                                        //ILogger<CreateProductCommandHandler> logger
+                                        ) 
+            : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+
+           // logger.LogInformation("CreateProductCommandHandler.Handle called with {@command}", command);
+/*
+            var result= await validator.ValidateAsync(command);
+            var errors= result.Errors.Select(x=> x.ErrorMessage).ToList();
+
+            if (errors.Any())
+            {
+                throw new ValidationException(errors.FirstOrDefault());
+            }
+*/
             var product = new Product
             {
                 Name = command.Name,
--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -1,14 +1,20 @@
-﻿
-namespace Catalog.API.Products.DeleteProduct
+﻿namespace Catalog.API.Products.DeleteProduct
 {
     public record DeleteProductCommand(Guid Id)
         : ICommand<DeleteProductResult>;
     public record DeleteProductResult(bool IsSuccess);
-    internal class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommandHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(command => command.Id).NotEmpty().WithMessage("Product Id is Reqired");
+        }
+    }
+    internal class DeleteProductCommandHandler(IDocumentSession session) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
     {
         public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
         {
-            logger.LogInformation("DeleteProductCommandHandler.Handle called with {@command}", command);
 
             //var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
             //if (product is null)
